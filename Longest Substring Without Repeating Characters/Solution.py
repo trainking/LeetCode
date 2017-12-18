b508568c02a8ec570e728b40370851831952b40d
yefulin <yfl@szxlzkj.com>
2017-12-18 17:20:01 +0800

    增加 Longest Substring Without Repeating Characters 解法

---
+++ Longest Substring Without Repeating Characters/Solution.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# use python 2.7
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        _max = 0
+        _mas = {}
+        j = 0 
+        for i in range(len(s)):
+            if _mas.has_key(s[i]):
+                j = max(j, _mas[s[i]] + 1)
+            _mas[s[i]] = i
+            _max = max(_max, i - j + 1)
+
+        return _max
+
+def main():
+    print Solution().lengthOfLongestSubstring("abcabcbb")
+
+if __name__ == '__main__':
+    main() 
