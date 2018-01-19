f02545cc4b2ce3f1de9d9838d34854d4c99e8970
yefulin <yfl@szxlzkj.com>
2018-01-19 13:31:25 +0800

    增加ZigZag的py

---
+++ ZigZag Conversion/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+
+
+def main():
+    print Solution().convert('PAYPALISHIRING', 3)
+
+if __name__ == '__main__':
+    main()
