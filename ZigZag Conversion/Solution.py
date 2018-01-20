61ba0f68baa11935ac1d6a0ea11ab9dd6364f957
yefulin <yfl@szxlzkj.com>
2018-01-20 14:30:40 +0800

    增加ZigZag的python解决方案

--- ZigZag Conversion/Solution.py
+++ ZigZag Conversion/Solution.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class Solution(object):
     def convert(self, s, numRows):
         """
@@ -5,6 +7,23 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        _map = [[] for x in xrange(numRows)]  # 结果集
+        _len = len(s)
+
+        i = 0
+        while(i < _len):
+            # 垂直添加
+            for idx in xrange(0,numRows):
+                if i < _len:
+                    _map[idx].append(s[i])
+                    i = i + 1
+            # 对角线添加
+            for idx in xrange(numRows - 2, 0, -1):
+                if i < _len:
+                    _map[idx].append(s[i])
+                    i = i + 1
+
+        return "".join(["".join(_map[x]) for x in xrange(numRows)])
 
 
 def main():
