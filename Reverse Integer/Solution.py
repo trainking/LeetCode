c7dbb0d33ba411c7494877ad7b5fb4150e8567f3
yefulin <yfl@szxlzkj.com>
2018-01-22 13:25:31 +0800

    增加Reverse Integet的最优解

--- Reverse Integer/Solution.py
+++ Reverse Integer/Solution.py
@@ -7,21 +7,14 @@ class Solution(object):
         :rtype: int
         """
         _t = str(abs(x))
-        result = 0
-        if x < 0:
-            result = -int(_t[::-1])
-            if (result < -214783648):
-                return 0
-        elif x > 0:
-            result = int(_t[::-1])
-            if result > 2147483647:
-                return 0
+        r = cmp(x,0)
+        result = int(_t[::-1])
 
-        return result
+        return r*result*(result < 2**31)
         
 
 def main():
-    print Solution().reverse(1534236469)
+    print Solution().reverse(12345)
 
 if __name__ == '__main__':
     main()
