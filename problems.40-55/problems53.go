abce0fbceeca2f49d8c0b8db4fbeba93d040f32e
trainking <yefulincode@gmail.com>
2021-12-26 22:16:27 +0800

    docs: 增加problems53

---
+++ problems.40-55/problems53.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func maxSubArray(nums []int) int {
+	return get(nums, 0, len(nums)-1).mSum
+}
+
+func pushUp(l, r Status) Status {
+	iSum := l.iSum + r.iSum
+	lSum := max(l.lSum, l.iSum+r.lSum)
+	rSum := max(r.rSum, r.iSum+l.rSum)
+	mSum := max(max(l.mSum, r.mSum), l.rSum+r.lSum)
+	return Status{lSum, rSum, mSum, iSum}
+}
+
+func get(nums []int, l, r int) Status {
+	if l == r {
+		return Status{nums[l], nums[l], nums[l], nums[l]}
+	}
+	m := (l + r) >> 1
+	lSub := get(nums, l, m)
+	rSub := get(nums, m+1, r)
+	return pushUp(lSub, rSub)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+type Status struct {
+	lSum, rSum, mSum, iSum int
+}
+
+func main() {
+	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+}
